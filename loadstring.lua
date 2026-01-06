17fbcc2ae7beb776c394cccb868c93b91fb59879
IsaacLunguinhoDev <isaackaluana@gmail.com>
2026-01-05 22:12:42 -0300

    loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/IsaacLunguinhoDev/Dev-777-hub-roblox-script/blob/main/script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IsaacLunguinhoDev/Dev-777-hub-roblox-script/refs/heads/main/script.lua"))()
